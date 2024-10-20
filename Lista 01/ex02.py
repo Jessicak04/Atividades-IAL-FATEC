59fa47925c94da0d832ab5ec89db21bfadf07349
Jessicak04 <jessica.karen04@gmail.com>
2024-10-20 14:41:10 -0300

    Arrumação no loop de volta ao inicio

--- Lista 01/ex02.py
+++ Lista 01/ex02.py
@@ -1,18 +1,18 @@
 while True:
-    # Solicita a entrada do usuário
+    # Solicita o número de entrada
     try:
         numero = int(input("Digite um número: "))
     except ValueError:
         print("Por favor, digite um número válido.")
         continue
     if numero > 100:
-        print("O número deve ser menor ou igual a 100.")
+        print("O número deve ser menor que 100.")
         continue
-        # Imprime os números de 3 em 3 a partir do número, até 100
+    #Faz a impresão dos números de 3 em 3 a partir do número, até 100
     for i in range(numero, 101, 3):
         print(i)
-        # Pergunta se o usuário deseja digitar outro número
-        resposta = input("Deseja digitar outro número? (sim/não): ")
-    if resposta != 'sim':
+    # Pergunta se o usuário deseja digitar outro número
+    resposta = input("Deseja digitar outro número? (sim/não): ")
+    if resposta.lower() != 'sim':
         print("Processo encerrado.")
         break
